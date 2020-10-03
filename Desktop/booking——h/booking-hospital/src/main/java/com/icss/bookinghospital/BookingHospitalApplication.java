e9bb7d852ac1c515405d408b73d4344205e16e08
Phoebe-feng <1163055562@qq.com>
2020-10-03 09:01:25 +0800

    项目框架
    Signed-off-by: Phoebe-feng <1163055562@qq.com>

---
+++ Desktop/booking——h/booking-hospital/src/main/java/com/icss/bookinghospital/BookingHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.icss.bookinghospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingHospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingHospitalApplication.class, args);
+    }
+
+}
---
+++ Desktop/booking——h/booking-hospital/src/test/java/com/icss/bookinghospital/BookingHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icss.bookinghospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingHospitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
